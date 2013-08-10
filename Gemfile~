3f941fd25ec6678df225088d273893f91d0031da
Ronnie Nyaga <rnafundi@gmail.com>
2013-08-10 17:36:41 +0300

    Added a User factory and tests for the show page

--- Gemfile~
+++ Gemfile~
@@ -35,6 +35,7 @@ end
 group :development, :test do
 	gem 'rspec-rails', '~>2.0'
 	gem 'annotate', '~>2.4.0'
+	gem 'factory_girl_rails', '~>4.2.1'
 end
 
 group :development do
---
+++ spec/factories.rb
@@ -0,0 +1,6 @@
+factory.define :user do |User|
+	user.name										"Ronnie Nyaga"
+	user.email									"rnafundi@gmail.com"
+	user.password								"foobar"
+	user.password_confirmation	"foobar"
+end
